abecfc8811c9c27df142a6b5482304b127ab1265
Nick <nhunt006@fiu.edu>
2017-11-13 21:50:36 -0500

    Improved Block Class Code Readability
    Detailed block class and improved code readability

--- Java Game Project 1/src/com/dev/main/Block.java
+++ Java Game Project 1/src/com/dev/main/Block.java
@@ -4,9 +4,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/*
+ * Block class represents in game surface.
+ */
 public class Block extends GameObject{
 
-	
+	// Block Constructor inherired from GameObject
 	public Block(float x, float y, ID id) 
 	{
 		super(x, y, id);
@@ -15,14 +18,17 @@ public class Block extends GameObject{
 	public void tick() 
 	{
 		// TODO implement color change effect when player near
+		// TODO handler collision through individual objects.
 	}
 
+	// Abstract Render method inherited from GameObject
 	public void render(Graphics g) 
 	{
 		if (id==ID.Block){g.setColor(Color.white);}
 		g.drawRect((int)x, (int)y, 32,32);
 	}
-	// 32 x 32 Rectangle
+	
+	// Get bounds of 32 x 32 Rectangle
 	public Rectangle getBounds() 
 	{
 		return new Rectangle((int)x,(int)y,32,32); 
